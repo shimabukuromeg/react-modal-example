18aab47847d485a49f44163fc87437e2a7bbc393
Shimabukuro Megumi <shimabukuromeg@gmail.com>
2022-01-30 11:07:28 +0900

    [update] modalのサンプル追加その1

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,25 @@
-import type { NextPage } from 'next'
+import type { NextPage } from 'next';
+import { useState } from 'react';
+import { Modal } from '../components/Modal1';
 
 const Home: NextPage = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <div>
-      Hello, World
+      <div>モーダルのサンプルその1</div>
+      <button type="button" onClick={() => setIsOpen(!isOpen)}>
+        {isOpen ? '閉じる' : '開く'}
+      </button>
+      <Modal size="small" open={isOpen} onClose={() => setIsOpen(!isOpen)}>
+        <div>
+          <div>Hello, Modal</div>
+          <button type="button" onClick={() => setIsOpen(!isOpen)}>
+            {isOpen ? '閉じる' : '開く'}
+          </button>
+        </div>
+      </Modal>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
